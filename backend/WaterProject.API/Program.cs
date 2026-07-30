7239fdbaa12f16d6dc585487bc5ded1ea89b9c29
Gaoussou Doucoure <gaousd4@gmail.com>
2026-07-30 00:43:53 -0700

    Set up frontend and backend to consume API

--- backend/WaterProject.API/Program.cs
+++ backend/WaterProject.API/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<WaterDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("WaterConnection")));
 
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x.WithOrigins("http://localhost:3000"));
 
 app.UseHttpsRedirection();
 
