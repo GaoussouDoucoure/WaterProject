92e964ae506039d7db9c64b4155aefadcf438215
Gaoussou Doucoure <gaousd4@gmail.com>
2026-07-29 23:24:27 -0700

    Install packages, build model, context and controller files to manipulate the data to show

--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353d7f7e02b058094bad38768cac9b407f299c52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5391b0d5ce0d0e43947e3df167112b78aff55d4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
